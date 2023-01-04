ea84d965865cc45f8f0cd0caf9fb723e3b0ec174
Emily Rohrbough <emilyrohrbough@yahoo.com>
2023-01-04 11:49:43 -0600

    .

--- .github/workflows/semantic-pull-request/validatePrTitle.js
+++ .github/workflows/semantic-pull-request/validatePrTitle.js
@@ -4,37 +4,30 @@ const changeCatagories = {
   breaking: {
     description: 'A breaking change that will require a MVB',
     title: '**Breaking Changes:**',
-    message: changeLinkPhrases.default,
   },
   dependency: {
     description: 'A change to a dependency that impact the user',
     title: '**Dependency Updates:**',
-    message: changeLinkPhrases.default,
   },
   deprecation: {
     description: 'A API deprecation notice for users',
     title: '**Deprecations:**',
-    message: changeLinkPhrases.default,
   },
   feat: {
     description: 'A new feature',
     title: '**Features:**',
-    message: changeLinkPhrases.default,
   },
   fix: {
     description: 'A bug or regression fix',
     title: '**Bugfixes:**',
-    message: changeLinkPhrases.fix,
   },
   misc: {
     description: ' misc user-facing change, like a UI update, which is not a fix or enhancement to how Cypress works',
     title: '**Misc:**',
-    message: changeLinkPhrases.default,
   },
   perf: {
     description: 'A code change that improves performance',
     title: '**Performance:**',
-    message: changeLinkPhrases.fix,
   },
 }
 
