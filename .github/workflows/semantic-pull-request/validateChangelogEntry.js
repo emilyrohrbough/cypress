82ae9d7be18b2545a833298e05a0b42a3e41167f
Emily Rohrbough <emilyrohrbough@yahoo.com>
2023-01-04 14:15:38 -0600

    and validate the changelog content

--- .github/workflows/semantic-pull-request/validateChangelogEntry.js
+++ .github/workflows/semantic-pull-request/validateChangelogEntry.js
@@ -11,7 +11,7 @@ function getResolvedMessage(type, prNumber, linkedIssues) {
 
     return `${issueMessage} ${links.join(',')}.`
   }
-  
+
   const prMessage = userFacingChanges[type].message.onlyPR
 
   return `${prMessage} [#${prNumber}](https://github.com/cypress-io/cypress/pulls/${prNumber}).`
@@ -39,7 +39,7 @@ module.exports = async function validateChangelogEntry({ github, restParameters,
   const hasChangeLogUpdate = pullRequestFiles.includes('cli/CHANGELOG.md')
 
   if (binaryFiles.length === 0) {
-    console.log("This PR does not contain changes that impacts the next Cypress release.")
+    console.log("This pull request does not contain changes that impacts the next Cypress release.")
     
     if (hasChangeLogUpdate) {
       console.log("Changelog entry is not required...")
@@ -51,7 +51,7 @@ module.exports = async function validateChangelogEntry({ github, restParameters,
   }
 
   if (!Object.keys(userFacingChanges).includes(semanticResult.type)) {
-    console.log("This PR does not contain user-facing changes that impacts the next Cypress release.")
+    console.log("This pull request does not contain user-facing changes that impacts the next Cypress release.")
 
     if (hasChangeLogUpdate) {
       console.log("Changelog entry is not required...")
@@ -67,5 +67,26 @@ module.exports = async function validateChangelogEntry({ github, restParameters,
     );
   }
 
-  // else verify changelog message!
+  const changelog = fs.readFileSync(dirname, '..', '..', 'cli', 'CHANGELOG.md')
+  // const nextVersion = await getNextVersionForPath()
+
+  // if (!changelog.includes(`## ${nextVersion}`)) {
+  //   throw new Error(`The changelog version does not contain the next Cypress version of ${nextVersion}. If the changelog version is correct, please correct the pull request title to correctly reflect the change being made.`)
+  // }
+
+  const sections = changelog.split(/**[a-zA-Z]+:**/)
+  if (!sections.includes(userFacingChanges[semanticResult.type].section)) {
+    throw new Error(`The changelog does not include the ${userFacingChanges[semanticResult.type].section} section. Given the pull request title provided, this section should be included in the changelog. If the changelog section is correct, please correct the pull request title to correctly reflect the change being made.`)
+  }
+
+  const resolveMessage = getResolvedMessage(semanticResult.type, restParameters.pull_number, linkedIssues)
+  if (!sections.includes(resolveMessage)) {
+    if (linkedIssues.length) {
+      throw new Error(`The changelog entry does not include the linked issues that this pull request resolves. Please update your entry to include:\n\n${resolveMessage}`)
+    }
+
+    throw new Error(`The changelog entry does not include the pull request link. Please update your entry to include:\n\n${resolveMessage}`)
+  }
+
+  console.log('It appears at a high-level your changelog entry is correct! the remaining validation is left to the pull request reviewers.')
 };
